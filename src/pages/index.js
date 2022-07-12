97e3d10259effa50e7729c864687fbcfee4034df
ZeoNish <motd.zeonish@gmail.com>
2022-07-12 10:20:05 +0300

    fix svg class

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro">
             Быстрый старт
-              <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-clock" width="24"
+              <svg xmlns="http://www.w3.org/2000/svg" className="" width="24"
                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                    stroke-linecap="round" stroke-linejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
